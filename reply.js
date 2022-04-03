bf747f015bff523863e0c1f75eac7c5bfbb735c9
ahmadzip <92171013+ahmadzip@users.noreply.github.com>
2022-04-03 11:09:24 +0700

    Update reply.js

--- reply.js
+++ reply.js
@@ -2,12 +2,6 @@ const answer = {
     "thanks":"Sama sama!",
     "p":"P Juga",
     "bot":"Hai",
-    "makasih":"Sama sama!",
-    "nuhun":"Sama sama!",
-    "assalamualaikum":"Waalaikumsalam wr.wb",
-    "assalamualaikum wr.wb":"Waalaikumsalam wr.wb",
-    "hi bot":"Hallo juga manusia",
-    "hallo":"Hallo juga manusia",
 }
 const notSpesificAnswer = {
     "thanks":"Sama sama!",
