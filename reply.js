30857e6abbe81f675ec527e39103c2aea9c57fac
ahmadzip <92171013+ahmadzip@users.noreply.github.com>
2022-04-03 11:00:47 +0700

    Auto Reply

---
+++ reply.js
@@ -0,0 +1,16 @@
+const answer = {
+    "thanks":"Sama sama!",
+    "p":"P Juga",
+    "bot":"Hai",
+    "makasih":"Sama sama!",
+    "nuhun":"Sama sama!",
+    "assalamualaikum":"Waalaikumsalam wr.wb",
+    "assalamualaikum wr.wb":"Waalaikumsalam wr.wb",
+    "hi bot":"Hallo juga manusia",
+    "hallo":"Hallo juga manusia",
+}
+const notSpesificAnswer = {
+    "thanks":"Sama sama!",
+    "hallo":"Hallo",
+}
+module.exports = {notSpesificAnswer, answer}
